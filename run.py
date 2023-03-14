f79e9c9f15906c7394163df3bf37bc46df9a0477
ofir <57630968+ofirfram@users.noreply.github.com>
2023-03-14 14:57:28 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import re
 import sys
 
 
-with open('extracted_data.txt', 'r') as f:
-    contents = f.read()
-    print(contents)
+# Access the value of the EXTRACTED_DATA environment variable
+extracted_data = os.environ.get('EXTRACTED_DATA')
+
+# Print the value of the extracted data
+print(f"The extracted data is: {extracted_data}")
