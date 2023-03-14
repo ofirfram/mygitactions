469a70118f0845b2b2fb4d0609c0fa5c288a73cd
ofir <57630968+ofirfram@users.noreply.github.com>
2023-03-14 15:04:03 +0200

    Update run.py

--- run.py
+++ run.py
@@ -8,3 +8,7 @@ extracted_data = os.environ.get('EXTRACTED_DATA')
 
 # Print the value of the extracted data
 print(f"The extracted data is: {extracted_data}")
+
+print(type(extracted_data))
+extracted_data = extracted_data.split()
+print(extracted_data)
