adaed0c7f800752d0a31fdb065fc24a2dc0b92bc
ofir <57630968+ofirfram@users.noreply.github.com>
2023-03-14 13:09:41 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,3 +3,9 @@ import os
 contents = os.getenv('contents')
 
 print(contents)
+
+contents = contents.split()
+print(contents)
+for line in contents:
+    if re.search("^SKIP_TESTS", line):
+         print("QPM_GUI " + line)
