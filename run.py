b5295f05a567429d63854ad040d7a9939355f5fb
ofir <57630968+ofirfram@users.noreply.github.com>
2023-03-14 15:09:19 +0200

    Update run.py

--- run.py
+++ run.py
@@ -5,9 +5,13 @@ import sys
 
 # Access the value of the EXTRACTED_DATA environment variable
 extracted_data = os.environ.get('EXTRACTED_DATA')
+print(extracted_data)
 
 extracted_data = extracted_data.split()
 print(extracted_data)
+print("ff")
+
+print("@@@@@@@@@") 
 
 for line in extracted_data:
   print(line + "\n")
