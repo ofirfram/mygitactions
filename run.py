a6e6d2ac11fbb0225dd7e9d61d9f9ad091110c9c
ofir <57630968+ofirfram@users.noreply.github.com>
2023-03-15 13:07:01 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,13 +7,10 @@ import sys
 extracted_data = os.environ.get('EXTRACTED_DATA')
 print(extracted_data)
 
-extracted_data = extracted_data.split()
-print(extracted_data)
-print("ff")
-
-print("@@@@@@@@@") 
-
-for line in extracted_data:
-  print(line + "\n")
-  if re.search("^SKIP_TESTS", line):
-      print("QPM_GUI " + line)
+pattern = r"SKIP_TESTS=.*?(?=[A-Z_]+=|$)"
+match = re.search(pattern, extracted_data)
+if match:
+    desired_str = match.group(0)
+    print(desired_str)
+else:
+    print("Desired string not found.")
