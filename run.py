5478e05431134968f607e0e8327bc1945277f907
ofir <57630968+ofirfram@users.noreply.github.com>
2023-05-03 15:50:23 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,14 +3,16 @@ import re
 import sys
 
 
-# Access the value of the EXTRACTED_DATA environment variable
-extracted_data = os.environ.get('EXTRACTED_DATA')
-print(extracted_data)
+# # Access the value of the EXTRACTED_DATA environment variable
+# extracted_data = os.environ.get('EXTRACTED_DATA')
+# print(extracted_data)
 
-pattern = r"SKIP_TESTS=.*?(?=[A-Z_]+=|$)"
-match = re.search(pattern, extracted_data)
-if match:
-    desired_str = match.group(0)
-    print(desired_str)
-else:
-    print("Desired string not found.")
+# pattern = r"SKIP_TESTS=.*?(?=[A-Z_]+=|$)"
+# match = re.search(pattern, extracted_data)
+# if match:
+#     desired_str = match.group(0)
+#     print(desired_str)
+# else:
+#     print("Desired string not found.")
+
+print("hello")
